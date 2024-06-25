e5a5a0ec4deab11477b330cc13b83e6403f29c75
sedica3340 <sedica3340@gmail.com>
2024-06-25 11:40:09 +0900

    변경

--- src/main/java/com/spring/jpastudy/chap01/entity/Product.java
+++ src/main/java/com/spring/jpastudy/chap01/entity/Product.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter @ToString
+@Getter @ToString(exclude = "nickName")
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,16 +20,17 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "prod_id")
-
     private Long id;
+
+    @Setter
     @Column(name = "prod_nm", length = 30, nullable = false)
-
     private String name;
+
     @Column(name = "price")
-
     private int price;
-    @Column(nullable = false)
 
+    @Setter
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Category category;
 
@@ -45,5 +46,16 @@ public class Product {
 
     public enum Category {
         FOOD, FASHION, ELECTRONIC
+    }
+
+    // 컬럼 기본값 설정
+    @PrePersist
+    public void prePersist() {
+        if (this.price == 0) {
+            this.price = 10000;
+        }
+        if(this.category == null) {
+            this.category = Category.FOOD;
+        }
     }
 }
